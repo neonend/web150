5d2d03c7c193eab2ea1b181572a8c56668e975d7
neonend <mkwiat01@seattlecentral.edu>
2014-01-16 20:04:21 -0800

    initial commit
    assignment 2 and sandbox commits

---
+++ js-sandbox/if-when.js
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+	<head>
+		<title>If/When</title>
+		<meta charset="utf8">
+	</head>
+<body>
+Oh, Javascript!
+<script>
+var age = prompt("How old are you?");
+age = Number(age);
+if (2014 - age > 2000){
+console.log("Lucky!");
+}
+</script>
+</body>
+</html>
