1048376971096a2e44d4459c9b345c38328a10b4
neonend <mkwiat01@seattlecentral.edu>
2014-01-23 20:52:18 -0800

    sandbox commit

---
+++ js-sandbox/royalewcheese.js
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<title></title>
+<meta charset=utf8>
+<body>
+<script src="http://thomaswilburn.net/sccc/data/SamuelJackson.js"></script>
+<script>
+
+</script>
+</body>
+</head>
+</html>
